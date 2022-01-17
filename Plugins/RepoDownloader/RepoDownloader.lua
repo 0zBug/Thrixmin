0967ea88c13dd03ecf7624acd44e0316e98776a8
Bug <77031032+0zBug@users.noreply.github.com>
2022-01-17 17:10:29 -0600

    Update RepoDownloader.lua

--- Plugins/RepoDownloader/RepoDownloader.lua
+++ Plugins/RepoDownloader/RepoDownloader.lua
@@ -2,7 +2,7 @@
 local DownloadRepo = loadstring(game:HttpGet("https://raw.githubusercontent.com/0zBug/RepoDownloader/main/main.lua"))()
 
 return {
-    {{"downloadrepo", "git", "repo"}, function(Args)
+    {{"downloadrepo", "git", "repo"}, "Downloads the chosen GitHub repo.", function(Args)
         spawn(function()
             DownloadRepo(Args[2])
         end)
