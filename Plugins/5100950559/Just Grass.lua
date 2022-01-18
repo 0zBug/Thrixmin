5e53ab19923d7bf0ba6df2bf2d197f0e43640133
Bug <77031032+0zBug@users.noreply.github.com>
2022-01-18 13:14:20 -0600

    Update Just Grass.lua

--- Plugins/5100950559/Just Grass.lua
+++ Plugins/5100950559/Just Grass.lua
@@ -2,8 +2,6 @@
 return {
     {"Outfit", "Removes your hats then adds the selected players hats.", function(Args)
         spawn(function()
-              local ghcmd = "-gh "
-        
               for i,v in pairs(game.Players.LocalPlayer.Character:GetDescendants()) do
                   if v:IsA("Accessory") then 
                       v:Remove()
@@ -11,10 +9,8 @@ return {
               end
 
               for i,v in pairs(game.Players:GetCharacterAppearanceInfoAsync(Args[2]).assets) do
-                  ghcmd = ghcmd .. tostring(v.id) .. ","
+                  game.Players:Chat(string.format("-gh %s", tostring(v.id))
               end
-
-              game.Players:Chat(ghcmd)
         end)
     end}
 }
