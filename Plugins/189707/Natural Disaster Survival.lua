784e7da178cbe6863141a766d5e00051c8437d45
Bug <77031032+0zBug@users.noreply.github.com>
2022-01-18 20:54:45 -0600

    Update Natural Disaster Survival.lua

--- Plugins/189707/Natural Disaster Survival.lua
+++ Plugins/189707/Natural Disaster Survival.lua
@@ -1,1 +1,11 @@
 
+return {
+    {{"Balloon", "GetBalloon"}, "Clones a balloon from someones character.", function(Args)
+        spawn(function() 
+            if game:FindFirstChild("GreenBalloon", true) then 
+            	Balloon = game:FindFirstChild("GreenBalloon", true):Clone()
+            	Balloon.Parent = game:GetService("Players").LocalPlayer.Backpack
+            end
+        end)
+    end}
+}
