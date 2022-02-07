bda3507261d5ab083214f7f5137085f384f3e836
Bug <77031032+0zBug@users.noreply.github.com>
2022-02-06 19:38:27 -0600

    Create ReportReasons.lua

---
+++ Assets/Dependencies/ReportReasons.lua
@@ -0,0 +1,87 @@
+return {
+    {
+        "Swearing", {
+            "They said fword in chat",
+            "Dude keeps swearing bruh ban him",
+            "help me he swore",
+            "Admin he keeps bypassing mean words",
+            "This user keeps swearing in the chat, please ban them",
+            "He kept saying racist stuff and the n word",
+            "Doesn't stop swearing when we told them to stop in chat",
+            "How is he not banned? He has sworn multiple times",
+            "Swearing in the chat, ban him"
+        }
+    },
+    {
+        "Inappropriate Username", {
+            "Their username has a swear word in it",
+            "The username is really weird and disgusting",
+            "He offended me by his racist username ban him",
+            "How does he have a mean username like that",
+            "Why is he walking around with a bad name that swears",
+            "Please ban this user he has a bad name"
+        }
+    },
+    {
+        "Bullying", {
+            "He followed me to games and bullied me with names and made me cry",
+            "Plz ban him admin he called me a bad word (gay)",
+            "They kept walking to me and making fun of me"
+        }
+    },
+    {
+        "Scamming", {
+            "they told me to go to a very shady website and i think it will hack my moms computer D:",
+            "they told me to go to 'gamesense.blog' which it sounds like a scam",
+            "they are telling me to go to grabify.ip/13aw876h and i dont want to",
+            "they are telling me to go to 'xhamster.com' and my friend is telling me its dangerous",
+            "they told me that they said i will get free robux if i goes to 'robux.gg.free/generator' and i dont want to :(",
+            "they told me to go to a roblox link to watch ads and get free robux",
+            "they told me to go to robloxscripts.com so i could start hackking"
+        }
+    },
+    {
+        "Dating", {
+            "ewww they are kissing",
+            "um i just found two boys dating and they on the bed..",
+            "what the helllllllllllll these people are ODing",
+            "hey i just saw two people dating on rolbox",
+            "excuse me i just found two girls and 3 boys dating help"
+        }
+    },
+    {
+        "Cheating/Exploiting", {
+            "i was playing and my stuff got flung",
+            "he kept flinging me out of the game",
+            "he killed me using a boombox or something",
+            "this user has somehow killed me idk how he said he uses synapse",
+            "how do i get someone banned when they deleted all my money on my game?",
+            "dude please help this user just killed me"
+        }
+    },
+    {
+        "Personal Question", {
+            "They askeed for my adress it is scaring me :(",
+            "They asked for my name and it scaring me alot :(",
+            "They asked can i come stay with you then said where i live and it scared me alot. :-(",
+            "They asked for my parents name D:",
+            "They asked how old i was and im uncomfortable. admin plz help me :-(",
+            "um he asked for my real name",
+            "how do i block them when they ask for my age",
+            "they just asked for my addres plz help xD",
+            "this guy keeps asking me what my real life name is and its uncomfortable",
+            "why does he keep asking for my adress"
+        }
+    },
+    {
+        "Offsite Links", {
+            "they told me to go to a very shady website and i think it will hack my moms computer D:",
+            "they told me to go to 'gamesense.blog' which it sounds like a scam",
+            "they are telling me to go to grabify.ip/13aw876h and i dont want to",
+            "they are telling me to go to 'xhamster.com' and my friend is telling me its dangerous",
+            "they told me that they said i will get free robux if i goes to 'robux.gg.free/generator' and i dont want to :(",
+            "they told me to go to a roblox link to watch ads and get free robux",
+            "they told me to go to robloxscripts.com so i could start hackking"
+        }
+    }
+}
